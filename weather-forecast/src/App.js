bd5b6691fb2e947984d1445c92d923b36020954c
Justin Zhou <justinzhou68@gmail.com>
2021-07-05 21:30:24 +1200

    added new styles

--- weather-forecast/src/App.js
+++ weather-forecast/src/App.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import Weather from './components/Weather';
+import { Dimmer, Loader } from 'semantic-ui-react';
 
 const {REACT_APP_API_URL, REACT_APP_API_KEY} = process.env
-const city = "Auckland"
+const defaultCity = "Auckland"
 function App() {
   //Creating two states
   const [lat, setLat] = useState([]);
@@ -21,7 +22,7 @@ function App() {
       
     });
 
-    await fetch(`${REACT_APP_API_URL}/weather?q=${city}&units=metric&appid=${REACT_APP_API_KEY}`)
+    await fetch(`${REACT_APP_API_URL}/weather?q=${defaultCity}&units=metric&appid=${REACT_APP_API_KEY}`)
     .then(res => res.json())
     .then(result => {
       setWeatherData(result)
@@ -32,7 +33,18 @@ function App() {
   },[lat, long]);
   return (
     <div className="App">
-      <Weather weatherData={weatherData}/>
+    {(typeof weatherData.main != 'undefined') ? (
+        <div>
+          <Weather weatherData={weatherData}/>
+         
+        </div>
+      ): (
+        <div>
+          <Dimmer active>
+            <Loader>Loading..</Loader>
+          </Dimmer>
+        </div>
+      )}
     </div>
   );
 }
--- weather-forecast/src/components/Weather.js
+++ weather-forecast/src/components/Weather.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './styles.css';
-import { Card } from 'semantic-ui-react'
+import moment from 'moment';
 
-const CardExampleCard = ({weatherData}) => (
-  <Card>
-    <Card.Content>
-        <Card.Header className="header">{weatherData.name}</Card.Header>
-    </Card.Content>
-  </Card>
+const WeatherCard = ({weatherData}) => (
+  <div className="main">
+      <p className="header">{weatherData.name}</p>
+      <div className="flex">
+        <p className="day">{moment().format('h:mm a')}</p>
+        <p className="day">{moment().format(' MMMM Do YYYY')}</p>
+        <p className="description">{weatherData.weather[0].main}</p>
+      </div>
+
+      <div className="flex">
+        <p className="temp"><span className="title">Temprature:</span> {weatherData.main.temp} &deg;C</p>
+        <p className="temp"><span className="title">Humidity:</span> {weatherData.main.humidity} %</p>
+      </div>
+
+      <div className="flex">
+        <p className="sunrise-sunset"><span className="title">Sunrise:</span> {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+        <p className="sunrise-sunset"><span className="title">Sunset:</span> {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+      </div>
+    
+  </div>
 )
 
-export default CardExampleCard;
+export default WeatherCard;
